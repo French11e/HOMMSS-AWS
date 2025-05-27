d7bd6a2f7864ded1f413b1056f263977a6a3eb64
HOMMSS Developer <hommss666@gmail.com>
2025-05-27 02:55:23 -0700

    Test Again

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -29,16 +29,14 @@
 
                         <div class="pb-3"></div>
 
-                        <div class="form-floating mb-3">
-                            <div class="position-relative">
-                                <input id="password" type="password" class="form-control form-control_gray @error('password') is-invalid @enderror" name="password" required=""
-                                    autocomplete="current-password" style="padding-right: 45px;">
-                                <label for="password">Password *</label>
-                                <button type="button" class="btn btn-link position-absolute" id="togglePassword"
-                                    style="right: 15px; top: 50%; transform: translateY(-50%); border: none; background: none; padding: 5px; z-index: 10; width: 30px; height: 30px; display: flex; align-items: center; justify-content: center;">
-                                    <i class="fa fa-eye" id="eyeIcon" style="color: #6c757d; font-size: 16px;"></i>
-                                </button>
-                            </div>
+                        <div class="form-floating mb-3 position-relative">
+                            <input id="password" type="password" class="form-control form-control_gray @error('password') is-invalid @enderror" name="password" required=""
+                                autocomplete="current-password" style="padding-right: 50px;">
+                            <label for="password">Password *</label>
+                            <button type="button" class="btn position-absolute" id="togglePassword"
+                                style="right: 12px; top: 50%; transform: translateY(-50%); border: none; background: none; padding: 0; z-index: 5; width: 24px; height: 24px; display: flex; align-items: center; justify-content: center;">
+                                <i class="fa fa-eye" id="eyeIcon" style="color: #6c757d; font-size: 14px;"></i>
+                            </button>
                             @error('password')
                             <span class="invalid-feedback" role="alert">
                                 <strong>{{ $message }}</strong>
